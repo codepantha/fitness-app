0722d3cb6ab36ca6af0c49e15645d39f6cac3a3a
codepanther <pezechigozirim@gmail.com>
2022-06-13 17:24:23 +0100

    create structure and style Navbar using mui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import ExerciseDetails from './pages/ExerciseDetails'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="exercises/:id" element={<ExerciseDetails />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
