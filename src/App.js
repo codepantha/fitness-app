9bd000ebddf79245a6bc71dce47cda12b01760fe
codepanther <pezechigozirim@gmail.com>
2022-06-14 14:10:29 +0100

    build and HeroBanner.js component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+
+import './App.css';
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import ExerciseDetails from './pages/ExerciseDetails'
