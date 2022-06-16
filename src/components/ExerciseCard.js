b32efcd7de55b6aae260057f853f224f6b801476
codepanther <pezechigozirim@gmail.com>
2022-06-16 11:39:28 +0100

    show Exercises

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Button, Stack, Typography } from '@mui/material';
+
+const ExerciseCard = ({ exercise }) => {
+  return (
+    <Box>
+      <Link className="exercise-card" to={`/exercises/${exercise.id}`}>
+        <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      </Link>
+
+      <Stack direction="row">
+        <Button sx={{ ml:'21px', color: '#fff', borderRadius:'20px', background:'#ffa9a9', fontSize: '14px', textTransform: 'capitalize' }}>
+          {exercise.bodyPart}
+        </Button>
+        <Button sx={{ ml:'21px', color: '#fff', borderRadius:'20px', background:'#fcc757', fontSize: '14px', textTransform: 'capitalize' }}>
+          {exercise.target}
+        </Button>
+      </Stack>
+
+      <Typography ml="21px" color="#000" fontWeight="bold" mt="11px" pb="10px" textTransform="capitalize" fontSize="24px">
+        {exercise.name}
+      </Typography>
+    </Box>
+  )
+}
+
+export default ExerciseCard
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Box, Typography } from '@mui/material';
+import ExerciseCard from './ExerciseCard';
 
-const Exercises = () => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
-    <div>Exercises</div>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: '110px' } }} mt="50px" p="20px">
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: '110px', sm: '50px' } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+    </Box>
+  );
+};
 
-export default Exercises
+export default Exercises;
